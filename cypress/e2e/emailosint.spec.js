08795ee7aa2e8f056ef1bae30b6fee8876621dad
ecgun3 <eceggunaydin@gmail.com>
2025-08-21 13:34:39 +0100

    emailosint.spec.js test dosyası eklendi

---
+++ cypress/e2e/emailosint.spec.js
@@ -0,0 +1,39 @@
+describe('EmailOSINT App End-to-End Tests', () => {
+
+  beforeEach(() => {
+    cy.visit('https://emailosint.onrender.com') // Ana sayfayı aç
+  })
+
+  it('Ana sayfa yükleniyor ve başlık doğru', () => {
+    cy.contains('Email OSINT').should('be.visible')
+  })
+
+  it('İkinci sayfaya gidip back butonu çalışıyor', () => {
+    cy.get('input[name="email"]').type('test@example.com')
+    cy.get('button[type="submit"]').click()
+    cy.url().should('include', '/results') // ikinci sayfaya gidildiğini doğrula
+    cy.get('button.back').click()          // back butonuna bas
+    cy.url().should('eq', 'https://emailosint.onrender.com/') // ana sayfaya dönmeli
+  })
+
+  it('GitHub ikonu doğru çalışıyor', () => {
+    cy.get('a.github-icon').should('have.attr', 'href').and('include', 'github.com')
+  })
+
+  it('Kiev JSON verisi doğru çalışıyor', () => {
+    cy.get('#kiev-json').then($el => {
+      const json = JSON.parse($el.text())
+      expect(json).to.have.property('city', 'Kiev')
+      expect(json).to.have.property('status', 'success')
+    })
+  })
+
+  it('BuiltWith API doğru çalışıyor', () => {
+    cy.get('#builtwith-result').then($el => {
+      const data = JSON.parse($el.text())
+      expect(data).to.have.property('technologies')
+      expect(data.technologies).to.be.an('array')
+    })
+  })
+
+})
